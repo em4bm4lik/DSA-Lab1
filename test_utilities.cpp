f2ab3a243853559dc0de1bba7dcd4654ac93a819
em4bm4lik <abdullahbinsaffi@gmail.com>
2025-09-13 15:35:50 +0500

    Modify testing mechanism

--- test_utilities.cpp
+++ test_utilities.cpp
@@ -2,7 +2,12 @@
 #include "utilities.h"
 using namespace std;
 
+// int main() {
+//     cout << "add(2,3) == 5: " << (add(2,3) == 5 ? "PASS" : "FAIL") << endl;
+//     return 0;
+// }
+
 int main() {
-    cout << "add(2,3) == 5: " << (add(2,3) == 5 ? "PASS" : "FAIL") << endl;
+    cout << "add(4,8) == 12: " << (add(4,8) == 12 ? "PASS" : "FAIL") << endl;
     return 0;
 }
